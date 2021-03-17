81679f34ca78f0ccf52dd44cffc592b7cc11af6b
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-03-17 23:40:30 +0200

    initializing the repo with micro stater.

---
+++ src/main/java/tariq/abdulghani/hello/HelloRestApplication.java
@@ -0,0 +1,13 @@
+package tariq.abdulghani.hello;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class HelloRestApplication extends Application {
+}
